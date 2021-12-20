628da0f8852f7553739c28615f631cc68f83ea67
ajinkya446 <ajinkya446@gmail.com>
2021-12-20 18:39:17 +0530

    Update raptorView.dart

--- lib/views/raptorView.dart
+++ lib/views/raptorView.dart
@@ -15,13 +15,7 @@ class RaptorButton extends StatefulWidget {
   final Color? color;
   final VoidCallback onSubmit;
   final Text buttonText;
-  RaptorButton(
-      {this.height,
-      this.width,
-      this.icon,
-      this.color,
-      required this.onSubmit,
-      required this.buttonText});
+  RaptorButton({this.height, this.width, this.icon, this.color, required this.onSubmit, required this.buttonText});
   @override
   _RapptorButtonState createState() => _RapptorButtonState();
 }
@@ -34,9 +28,7 @@ class _RapptorButtonState extends State<RaptorButton> {
       child: Container(
         height: widget.height ?? MediaQuery.of(context).size.height * 0.05,
         width: widget.width ?? MediaQuery.of(context).size.width * 0.26,
-        decoration: BoxDecoration(
-            color: widget.color ?? Colors.blue,
-            borderRadius: BorderRadius.circular(20)),
+        decoration: BoxDecoration(color: widget.color ?? Colors.blue, borderRadius: BorderRadius.circular(20)),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -69,8 +61,7 @@ class RaptorDialog extends StatefulWidget {
   State<StatefulWidget> createState() => RaptorDialogState();
 }
 
-class RaptorDialogState extends State<RaptorDialog>
-    with SingleTickerProviderStateMixin {
+class RaptorDialogState extends State<RaptorDialog> with SingleTickerProviderStateMixin {
   late AnimationController controller;
   late Animation<double> scaleAnimation;
   late Animation<double> height, width;
@@ -79,20 +70,12 @@ class RaptorDialogState extends State<RaptorDialog>
   void initState() {
     super.initState();
 
-    controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 450));
-    scaleAnimation =
-        CurvedAnimation(parent: controller, curve: Curves.elasticInOut);
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 450));
+    scaleAnimation = CurvedAnimation(parent: controller, curve: Curves.elasticInOut);
 
-    height = TweenSequence(<TweenSequenceItem<double>>[
-      TweenSequenceItem<double>(
-          tween: Tween(begin: 10, end: widget.size.height * 0.25), weight: 50)
-    ]).animate(controller);
+    height = TweenSequence(<TweenSequenceItem<double>>[TweenSequenceItem<double>(tween: Tween(begin: 10, end: widget.size.height * 0.25), weight: 50)]).animate(controller);
 
-    width = TweenSequence(<TweenSequenceItem<double>>[
-      TweenSequenceItem<double>(
-          tween: Tween(begin: 10, end: widget.size.width * 0.7), weight: 50)
-    ]).animate(controller);
+    width = TweenSequence(<TweenSequenceItem<double>>[TweenSequenceItem<double>(tween: Tween(begin: 10, end: widget.size.width * 0.7), weight: 50)]).animate(controller);
     controller.addListener(() {
       setState(() {});
     });
@@ -118,17 +101,13 @@ class RaptorDialogState extends State<RaptorDialog>
               child: Container(
                 height: height.value,
                 width: width.value,
-                decoration: ShapeDecoration(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0))),
+                decoration: ShapeDecoration(color: Colors.white, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0))),
                 child: height.value == widget.size.height * 0.25
                     ? Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           CircularProgressIndicator(
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(Colors.red),
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
                           ),
                           SizedBox(
                             height: 20,
@@ -152,93 +131,3 @@ class RaptorDialogState extends State<RaptorDialog>
     return false;
   }
 }
-
-
-
-
- // Container(
-            //     height: 200,
-            //     child: Hero(
-            //       tag: 'flutterLogo',
-            //       child: GestureDetector(
-            //         onTap: () {
-            //           Navigator.push(
-            //               context,
-            //               MaterialPageRoute(
-            //                   builder: (BuildContext context) => Details()));
-            //         },
-            //         child: FlutterLogo(
-            //           size: 100,
-            //         ),
-            //       ),
-            //     )),
-            // AnimatedContainer(
-            //     color: colorContainer,
-            //     height: height,
-            //     duration: Duration(seconds: 1),
-            //     child: TweenAnimationBuilder(
-            //       tween: Tween<double>(begin: 0, end: 1),
-            //       duration: Duration(seconds: 2),
-            //       child: ListView.builder(
-            //           itemCount: 10,
-            //           itemBuilder: (context, index) {
-            //             return Container(
-            //               height: 60,
-            //               child: Card(
-            //                 color: Colors.blue,
-            //                 child: ListTile(
-            //                   title: Text('$index'),
-            //                   trailing: Text("Price: 1200.00 Rs"),
-            //                   leading: AnimatedBuilder(
-            //                       animation: controller,
-            //                       builder: (BuildContext context, _) {
-            //                         return IconButton(
-            //                           onPressed: () {
-            //                             statusAnimation
-            //                                 ? controller.reverse()
-            //                                 : controller.forward();
-            //                             setState(() {});
-            //                           },
-            //                           icon: Icon(
-            //                             Icons.home,
-            //                             size: sizeAnimation.value,
-            //                             color: colorAnimation.value,
-            //                           ),
-            //                         );
-            //                       }),
-            //                 ),
-            //               ),
-            //             );
-            //           }),
-            //       builder: (context, double value, Widget child) {
-            //         return Opacity(
-            //           opacity: value,
-            //           child: Padding(
-            //             padding: EdgeInsets.only(top: value * 20),
-            //             child: child,
-            //           ),
-            //         );
-            //       },
-            //     )),
-
-            // Center(
-      //     child: RaptorButton(
-      //   onSubmit: () => raptorDialogView(
-      //       context: context,
-      //       dialogText: Text(
-      //         "please Wait",
-      //         style: TextStyle(fontSize: 20, color: Colors.blue),
-      //       )),
-      //   buttonText: Text("Demo"),
-      // )
-      //     child: RaptorLoading(
-      //   loadCount: 3,
-      //   shrinkWrap: true,
-      //   listDetail: data,
-      //   itemBuilder: (context, index) {
-      //     return ListTile(
-      //         trailing: Icon(Icons.time_to_leave),
-      //         title: Text((index + 1).toString()));
-      //   },
-      // )
-      // ),
